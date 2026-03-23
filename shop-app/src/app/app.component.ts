b8415985ec74e9cced880646396f7b14e180f34a
aroksaana <aleksandrlubenskij04@gmail.com>
2026-03-23 20:40:13 +0200

    modify some files

--- shop-app/src/app/app.component.ts
+++ shop-app/src/app/app.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';       // для router-outlet
+import { RouterModule } from '@angular/router';
+
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { ListComponent } from './features/products/list/list.component'; // 👈 ДОДАЙ
 
 @Component({
-  selector: 'shop-root',          // цей селектор має збігатися з index.html
+  selector: 'shop-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   imports: [
     HeaderComponent,
     FooterComponent,
-    RouterModule               // без цього router-outlet не працює
+    ListComponent,   // 👈 ОБОВ’ЯЗКОВО
+    RouterModule
   ]
 })
 export class AppComponent {}
--- shop-app/src/app/shared/components/card/card.component.ts
+++ shop-app/src/app/shared/components/card/card.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // 👈 ДОДАТИ
 import { Product } from '../../models/product';
 
 @Component({
   selector: 'shop-card',
   standalone: true,
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
+  imports: [CommonModule] // 👈 ОБОВ'ЯЗКОВО
 })
 export class CardComponent {
-  @Input({ required: true }) item!: Product;  // обов'язковий @Input
+  @Input({ required: true }) item!: Product;
 }
